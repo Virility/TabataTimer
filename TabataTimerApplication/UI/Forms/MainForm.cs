3d837f7857fcb0da072e11cfd57fb52979e58540
Virility <marcovquez1995@gmail.com>
2018-03-07 18:06:47 -0800

    Update MainForm.cs

--- TabataTimerApplication/UI/Forms/MainForm.cs
+++ TabataTimerApplication/UI/Forms/MainForm.cs
@@ -63,6 +63,14 @@ namespace TabataTimerApplication.UI.Forms
                 _tabataTimer.OnFinished += OnFinished;
 
                 _mainLogProvider.Log(LogItem.Create("Started timer."));
+                var entries = new[]
+                {
+                    new LogEntry("Expected to take ", true, false),
+                    new LogEntry($"{(_tabataTimer.TotalWorkoutTime + _tabataTimer.PreparationTime).TotalSeconds:N0}", false, false, SystemColors.Highlight),
+                    new LogEntry(" seconds.", false),
+                };
+                _mainLogProvider.Log(LogItem.Create(entries));
+
                 await _tabataTimer.Start();
             }
             else
